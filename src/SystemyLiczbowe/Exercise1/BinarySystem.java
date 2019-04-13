7fbbf9f8f8b09761321493f1c07fbba78302a188
KacperSzuba <38376776+KacperSzuba@users.noreply.github.com>
2019-04-13 15:38:33 +0200

    First exercise

--- src/SystemyLiczbowe/Exercise1/BinarySystem.java
+++ src/SystemyLiczbowe/Exercise1/BinarySystem.java
@@ -1,4 +1,4 @@
-package SystemyLiczbowe.Zadanie1;
+package SystemyLiczbowe.Exercise1;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class SystemDwojkowy {
+public class BinarySystem {
     public static void main(String[] args) throws IOException {
         Path inputPath = Paths.get("C:\\Users\\kacpe\\OneDrive\\Pulpit\\inf-pr-dane\\dane\\58\\dane_systemy1.txt");
         List<String> list = Files.readAllLines(inputPath, Charset.forName("UTF-8"));
@@ -16,7 +16,7 @@ public class SystemDwojkowy {
         for (int i=0;i<list.size();i++){
             String line = list.get(i);
             String[] tab = line.split(" ");
-            for(int q=tab[1].length()-1;q>0;q--){
+            for(int q=tab[1].length()-1;q>=0;q--){
                     if (tab[1].charAt(q) == '1') {
                         temp += power;
                         power *= 2;
@@ -34,8 +34,8 @@ public class SystemDwojkowy {
             temp=0;
             power = 1;
         }
-        System.out.println(theLowest);
-        Path outputPath = Paths.get("C:\\Users\\kacpe\\OneDrive\\Pulpit\\inf-pr-dane\\dane\\58\\wynik1.txt");
+        System.out.println(result);
+        Path outputPath = Paths.get("C:\\Users\\kacpe\\OneDrive\\Pulpit\\inf-pr-dane\\dane\\58\\Results\\BinarySystemResult.txt");
         Files.write(outputPath,result.getBytes());
     }
 }
